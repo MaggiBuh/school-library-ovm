984c4125d9ba4404dbb6001cfb9ef078758a3654
Max Hellige <max.hellige@plentymarkets.com>
2018-04-16 11:15:25 +0200

    add new angular 2 project

---
+++ src/app/startpage.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'start-page',
+    template: require('./startpage.html'),
+    styles:   [require('./startpage.scss')]
+})
+export class StartPageComponent
+{
+
+}
