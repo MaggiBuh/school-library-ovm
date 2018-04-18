a77cf1d3363a3b15f938e98f6e586f1f1c594cce
Max Hellige <max.hellige@plentymarkets.com>
2018-04-19 01:27:12 +0200

    add helper to open php files with http post/get

---
+++ src/app/view/php-connection-helper/php-connection-helper.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import {
+    Headers,
+    Http
+} from '@angular/http';
+
+@Injectable()
+export class PhpConnectionHelper
+{
+
+    private _header:any = new Headers();
+
+    constructor(private _http:Http)
+    {
+    }
+
+    public loginWithExistingAccount(userName:string, password:string):Observable<any>
+    {
+        let url:string = 'assets/php/login/login.php';
+        return this._http.post(
+            url,
+            {
+                userName: userName,
+                password: password
+            },
+            {
+                headers: this._header
+            }
+        );
+    }
+
+}
