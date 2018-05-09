9c4332658f83e5119e3921e95da1d70f688c0b99
Max Hellige <max.hellige@plentymarkets.com>
2018-05-09 10:08:13 +0200

    init global book data

--- src/app/plugin-terra-basic.component.ts
+++ src/app/plugin-terra-basic.component.ts
@@ -1,14 +1,29 @@
-import { Component } from '@angular/core';
+import {
+    Component,
+    OnInit
+} from '@angular/core';
+import { BookDataService } from './view/book-data-service/book-data.service';
 
 @Component({
     selector: 'plugin-terra-basic-app',
     template: require('./plugin-terra-basic.component.html'),
     styles:   [require('./plugin-terra-basic.component.scss')],
 })
-export class PluginTerraBasicComponent
+export class PluginTerraBasicComponent implements OnInit
 {
-    public constructor()
+    public constructor(private _bookDataService:BookDataService)
     {
 
     }
+
+    public ngOnInit():void
+    {
+        this.initGlobalBookData();
+    }
+
+    private initGlobalBookData():void
+    {
+        this._bookDataService.setBookData();
+    }
+
 }
